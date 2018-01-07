70cd5b93bc25def634f9af1d9e94933304ea614a
pawel <pawel>
2018-01-07 15:06:11 +0100

    pubsub scenario with python

---
+++ microservices/python/pubsub_consumer.py
@@ -0,0 +1,41 @@
+'''
+Created on Jan 7, 2018
+
+@author: pawel
+
+kafka pub/sub scenario
+'''
+
+import json
+import os
+from kafka import KafkaConsumer
+
+
+# kafa specific settings
+TOPIC_NAME = 'pubsub_topic'
+BOOTSTRAP_SERVERS = os.environ['BOOTSTRAP_SERVERS']
+AUTO_OFFSET_RESET = 'latest' # 'latest/earliest' -> default = latest
+
+
+
+consumer = KafkaConsumer(bootstrap_servers=BOOTSTRAP_SERVERS,
+                         auto_offset_reset = AUTO_OFFSET_RESET)
+available_topics = consumer.topics()
+
+#subscribe dynamically to desired topic
+for topic in available_topics:
+    if topic == TOPIC_NAME:
+        print('found desired topic')
+        consumer.subscribe(topics = [topic])
+        print('subscribed to topic %s' % topic)
+        
+        for msg in consumer:
+            encoded_value = msg.value
+            decoded_value = encoded_value.decode('utf-8')
+            value = json.loads(decoded_value)
+
+            # add extra info to message
+            value['partition'] = msg.partition
+            value['offset'] = msg.offset
+            
+            print('received', value)
