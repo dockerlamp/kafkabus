7e1263e3cc30ddec803080e8cf9e673508f2aa52
pawel <pawel>
2018-01-30 00:16:25 +0100

    node: command producer in async/await manner

--- microservices/node/producer.js
+++ microservices/node/producer.js
@@ -3,15 +3,14 @@ var kafka = require('kafka-node');
 var bus = require("../../systembus/bus.json");
     commands_cfg = require("../../systembus/commands.json");
 
-
 // test env settings
 var command_names = Object.keys(commands_cfg)
     COMMAND_NAME = command_names[Math.floor(Math.random()*command_names.length)];
     COMMAND_CFG = commands_cfg[COMMAND_NAME]
     OBJ_ID = Math.floor(Math.random() * (9*Math.pow(10,4) - 8*Math.pow(10,4)) + 8*Math.pow(10,4));
     PRODUCER_ID = Math.floor(Math.random() * (2*Math.pow(10,3) - Math.pow(10,3)) + Math.pow(10,3));
-    MSG_NO_RANGE = [Math.pow(10,6), 2*Math.pow(10,6)]
-    DELAY = 2000
+    MSG_NO_RANGE = [Math.pow(10,6), 2*Math.pow(10,6)];
+    DELAY = 1000
 
 
 // kafka settings
@@ -20,35 +19,44 @@ var BOOTSTRAP_SERVERS = bus['bootstrap_servers']
     KEY = (COMMAND_CFG['key'] == "None") ? "None" : COMMAND_CFG['key']+'.'+OBJ_ID
 
 
-console.log(COMMAND_NAME, OBJ_ID, PRODUCER_ID, MSG_NO_RANGE)
-console.log(BOOTSTRAP_SERVERS, TOPIC_NAME, KEY)
-
-var client = new kafka.KafkaClient({kafkaHost: BOOTSTRAP_SERVERS});
-    producer = new kafka.Producer(client);
-    msg_no = MSG_NO_RANGE[0];
+const delay = (duration) =>
+    new Promise(resolve => setTimeout(resolve, duration));
 
 
-    producer.on('ready', function () {
-    setInterval(send, DELAY);
+function whenProducerReady() {
+    return new Promise(function (resolve, reject) {
+        var client = new kafka.KafkaClient({kafkaHost: BOOTSTRAP_SERVERS});
+            producer = new kafka.Producer(client);
+        producer.on('ready', function () {
+            resolve(producer);
+        });
     });
-    
-    producer.on('error', function (err) {
-    console.log('error', err);
-    });
+}
 
-function send () {
-    var msg = JSON.stringify({
-        'uuid' : Math.floor(Math.random() * (6*Math.pow(10,5) - 5*Math.pow(10,5)) + 5*Math.pow(10,5)),
-        'source' : PRODUCER_ID,
-        'msg' : msg_no,
-        'command_name' : COMMAND_NAME,
-    })
-    producer.send([
-        {topic: TOPIC_NAME, messages: [msg]}
-    ], function (err, data) {
-        if (err) console.log(err);
-        else console.log('sent', msg);
-        ++msg_no
-        if (msg_no === MSG_NO_RANGE[1]) process.exit();
-    });
+async function startProducingCommands() {
+    let producer = await whenProducerReady();
+        msg_no = MSG_NO_RANGE[0];
+        
+
+    while(msg_no <= MSG_NO_RANGE[1]) {
+        var msg = JSON.stringify({
+            'uuid' : Math.floor(Math.random() * (6*Math.pow(10,5) - 5*Math.pow(10,5)) + 5*Math.pow(10,5)),
+            'source' : PRODUCER_ID,
+            'msg' : msg_no,
+            'command_name' : COMMAND_NAME,
+        })
+
+        payload = {topic: TOPIC_NAME, messages: [msg]};
+        if (KEY == 'None') {payload['key'] = KEY};
+
+        producer.send([payload], 
+            function (err, data) {
+                if (err) console.log(err);
+                else console.log('sent', msg);
+                ++msg_no
+        });
+        await delay(DELAY);
     }
+    console.log('command producer has finished.');
+}
+startProducingCommands()
