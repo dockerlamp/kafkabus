532d38d9c4c4d81978eb3bba50fb98ecc7c7fa1a
pawel <pawel>
2018-02-01 21:51:13 +0100

    node: event emiter added

--- microservices/node/command_handlers/add_environ.js
+++ microservices/node/command_handlers/add_environ.js
@@ -2,16 +2,22 @@ var commands_cfg = require("../../../systembus/commands.json");
     events_cfg = require("../../../systembus/events.json");
     handlers_cfg = require("../../../systembus/handlers.json");
 
+    emit = require('../event_producer.js').emit;
 
 function addEnviron(command) {
-    console.log('handling command', command);
+    console.log('handling command', JSON.stringify(command));
     // TODO do stuff here...
 
     var command_name = command['command_name']
         handler_name = commands_cfg[command_name]['handler']
 
     handlers_cfg[handler_name]['event_names'].forEach(function(event_name){
-        console.log(event_name);
+        event = {
+            'event_name' : event_name,
+            'uuid' : command['uuid'],
+            'source' : command['source'],
+            }
+        emit(event_name, event)
     });
 }
 
--- microservices/node/event_producer.js
+++ microservices/node/event_producer.js
@@ -1,6 +1,35 @@
 var kafka = require('kafka-node');
 
 var bus = require("../../systembus/bus.json");
-    commands_cfg = require("../../systembus/commands.json");
+    events_cfg = require("../../systembus/events.json");
 
-// TODO prepare event emiter...
+// kafka settings
+var BOOTSTRAP_SERVERS = bus['bootstrap_servers']
+
+
+function whenProducerReady() {
+    return new Promise(function (resolve, reject) {
+        var client = new kafka.KafkaClient({kafkaHost: BOOTSTRAP_SERVERS});
+            producer = new kafka.Producer(client);
+        producer.on('ready', function () {
+            resolve(producer);
+        });
+    });
+}
+
+
+async function emit(event_name, event) {
+
+    var producer = await whenProducerReady();
+    event_cfg = events_cfg[event_name]
+    payload = {topic:  event_cfg['topic'], messages: [event]};
+    if (event_cfg['key']!= 'None') {payload['key'] = event_cfg['key']};
+
+    producer.send([payload], 
+        function (err, data) {
+            if (err) console.log(err);
+            else console.log('raised event', event);
+    });
+}
+
+exports.emit = emit
