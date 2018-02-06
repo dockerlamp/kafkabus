267fb8359717b7de89f7ee7d5e30325b58ffb823
pawel <pawel>
2018-02-06 22:32:26 +0100

    skeleton for event emiting mechanism

--- microservices/py2/systembus/command_handlers/create_container.py
+++ microservices/py2/systembus/command_handlers/create_container.py
@@ -6,7 +6,17 @@ Created on Jan 26, 2018
 
 import json
 
+events_cfg = json.load(open('systembus/events.json'))
+
 
 def createContainerCommandHandler(command):
     print('handling command', command)
     # TODO do stuff here...
+
+    # get event names to emit
+    command_name = command['command_name']
+    event_names = [event_name for event_name, cfg in events_cfg.items() \
+                                    if command_name in cfg['on_behalf_of']]
+    for event_name in event_names:
+        # TODO emit event
+        pass
