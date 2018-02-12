5db9785a70b3459699e8f3b945b621d35cf3647b
pawel <pawel>
2018-02-12 01:31:20 +0100

    python: eventbus implementation

---
+++ microservices/python/systembus/eventbus.py
@@ -0,0 +1,48 @@
+import json
+
+from ibus import IBus
+from kafkabus import KafkaBus
+
+# platform settings
+bus_config = json.load(open('../../../systembus/bus.json'))
+
+
+class EventBus(IBus, KafkaBus):
+    '''singleton'''
+    _instance = None
+
+    def __new__(cls, *args, **kwargs):
+        if not cls._instance:
+            cls._instance = super(EventBus, cls).__new__(cls)
+        return cls._instance
+
+
+    def __init__(self, events_config = None):
+        super().__init__(bus_config)
+        if events_config:
+            self.events_config = events_config
+
+        if not hasattr(self, 'event_handlers'):
+            self.event_handlers = {}
+
+
+    async def add_handler(self, name, event_handler):
+        if name in self.event_handlers:
+            raise ValueError('Event handler %s already registered!')
+        self.event_handlers[name] = event_handler
+
+
+    async def send(self, event):
+        topic_name = self.command_handlers[event['command_name']]['topic']
+        topic_key = self.command_handlers[event['command_name']]['key']
+        topic_key = None if topic_key == 'None' else topic_key 
+
+        KafkaBus.__enter__() # ensure kafka producer is ready
+        KafkaBus._producer.send(topic = topic_name, key = bytes(topic_key, 'utf-8') if topic_key else topic_key,\
+            value = bytes(json.dumps(event), 'utf-8'))
+        return
+
+
+    async def consume_from(self, topic, group):
+        # TODO use "channels" attrib from bus.json instead of topic and group?
+        return await KafkaBus._get_consumer((topic, group))
--- microservices/python/systembus/testbus.py
+++ microservices/python/systembus/testbus.py
@@ -5,20 +5,29 @@ from sys import path
 path.append('../')
 
 import event_handlers
+import command_handlers
 from commandbus import CommandBus
+from eventbus import EventBus
 
 # platform settings
 commands_config = json.load(open('../../../systembus/commands.json'))
+events_config = json.load(open('../../../systembus/events.json'))
 
 
-with CommandBus(commands_config) as cb1, CommandBus() as cb2:
+with CommandBus(commands_config) as cb1, CommandBus() as cb2, EventBus(events_config) as eb:
     print(cb1, cb2)
     print(id(cb1.bus_config), id(cb2.bus_config))
 
-    handlers = cb1.load_handlers(event_handlers)
-    print(handlers)
+    chandlers = cb1.load_handlers(command_handlers)
+    print(chandlers)
 
-    for name, handler in handlers.items():
+    for name, handler in chandlers.items():
         cb1.add_handler(name, handler)
 
     print(id(cb1.command_handlers), id(cb2.command_handlers))
+
+    ehandlers = eb.load_handlers(event_handlers)
+    print(ehandlers)
+
+    for name, handler in ehandlers.items():
+        eb.add_handler(name, handler) 
